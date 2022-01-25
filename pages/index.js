817e1a431bbc8b0b79fa37e5e1f3431b521108f9
sulamita_althaus <sulamitaalthaus44@gmail.com>
2022-01-25 10:12:19 -0300

    create ChatPage

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,13 @@
 import { Box, Button, Image, Text, TextField } from '@skynexui/components';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Title(props) {
     const Tag = props.tag || 'h1';
     return (
         <>
-            <GlobalStyle/>
             <Tag>{props.children}</Tag>
             <style jsx>{`
                 ${Tag} {
@@ -47,11 +20,11 @@ function Title(props) {
 }
 
 export default function PaginaInicial() {
-  const username = 'sulamitaalthaus';
+  const [username, setUsername] = useState('sulamitaalthaus')
+  const router = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -78,6 +51,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push('/chat');
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -90,6 +67,8 @@ export default function PaginaInicial() {
 
             <TextField
               fullWidth
+              value={username}
+              onChange={e => setUsername(e.target.value)}
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
